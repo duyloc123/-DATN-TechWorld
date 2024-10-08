3d3c1e1e1cf21eeac9d4265d41a93a416ca7c5ae
duyloc123 <loc123456000@gmail.com>
2024-10-08 21:20:07 +0700

    Template Homepage

---
+++ Template/main.js
@@ -0,0 +1,51 @@
+// Burger menus
+document.addEventListener('DOMContentLoaded', function() {
+    // open
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
+    const logoHeader = document.getElementById('Header-Logo');
+
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    // close
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function() {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+});
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.pageYOffset > 0) {
+        navbar.classList.add('shadow-md', 'bg-white');
+        navbar.classList.remove('bg-gray-100');
+    } else {
+        navbar.classList.remove('shadow-md', 'bg-white');
+        navbar.classList.add('bg-gray-100');
+    }
+});
